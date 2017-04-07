92ed976514e864579493f9cea0526e203805997e
Thiago Tonon <thiagotonon2@gmail.com>
2017-04-07 19:34:45 -0300

    Testing email authentication

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,8 +3,8 @@ class ContactMailer < ApplicationMailer
   def message_mail(contact)
     @contact = contact
     subject = "Nova mensagem - #{@contact.subject}"
-    # mail(to: "thiagotonon2@gmail.com", subject: subject)
-    mail(to: "atendimento@ibta.edu.br", subject: subject)
+    mail(to: "thiagotonon2@gmail.com", subject: subject)
+    # mail(to: "atendimento@ibta.edu.br", subject: subject)
   end
 
 end
