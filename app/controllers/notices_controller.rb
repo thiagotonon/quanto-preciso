de08f517777b5770dffef9529072d39ee0a78f9a
Thiago Tonon <thiagotonon2@gmail.com>
2017-05-02 11:03:39 -0300

    Changed main Notice order

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,7 +1,7 @@
 class NoticesController < ApplicationController
 
   def index
-    @notices = Notice.order(id: :desc).limit(10)
+    @notices = Notice.order(posted_at: :desc).limit(5)
     render json: @notices, each_serializer: Notice::IndexSerializer
   end
 
